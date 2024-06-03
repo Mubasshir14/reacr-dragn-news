4982f80c2ee12256a878200835b6bb1d40acf651
Mubasshir Al Kasshaf <mubasshiralkasshaf22@gmail.com>
2024-06-03 15:15:09 +0600

    left side

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() =>{
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    },[])
     return (
-        <div>
-            <h2 className="text-2xl">Left Side Nav</h2>
+        <div className="p-2">
+            <h2 className="text-xl font-bold mb-3">All Category</h2>
+            <div className="bg-gray-200 rounded-lg flex items-center justify-center p-3 font-bold mb-3">
+                National News
+            </div>
+            {
+                categories.map(category => <Link className="block  lg:pl-14 mb-3 text-[#757575] font-bold" key={category.id} to={`/category/${category.id}`}>{category.name}</Link>)
+            }
         </div>
     );
 };
